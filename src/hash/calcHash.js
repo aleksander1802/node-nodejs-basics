4486ede8397210b870b79f3974ec6ccf0cd7580c
Alex2 <aleksander1802@gmail.com>
2023-06-14 22:38:01 +0300

    feat: hash completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHmac } from 'node:crypto';
+import path from 'path';
+import fs from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve('src/hash/files/fileToCalculateHashFor.txt');
+
+  try {
+    const secret = await fs.readFile(pathToFile, 'utf8');
+
+    const hash = createHmac('sha256', secret).digest('hex');
+
+    console.log(hash);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error();
+    }
+  }
 };
 
 await calculateHash();
