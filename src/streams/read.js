dfc9fbcb6186da91727646634af04b0f496c28c8
Alex2 <aleksander1802@gmail.com>
2023-06-15 02:10:59 +0300

    feat: streams completed

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { stdout } from 'process';
+import path from 'path';
+
+async function read() {
+  const pathToFile = path.resolve('src/streams/files/fileToRead.txt');
+  const pipelineAsync = promisify(pipeline);
+  const fileReadStream = createReadStream(pathToFile);
+
+  try {
+    await pipelineAsync(fileReadStream, stdout);
+  } catch (error) {
+    if (error) {
+      console.error(`An error occurred: ${error}`);
+    }
+  }
+}
 
 await read();
