9930ac9223a083412ac21212a9586279eb44c62b
Alex2 <aleksander1802@gmail.com>
2023-06-15 23:36:16 +0300

    feat: updated cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,13 @@ const parseArgs = () => {
   const args = process.argv.slice(2);
 
   for (let i = 0; i < args.length; i += 2) {
-    console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+    if (args[i].startsWith('--')) {
+      console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+    } else if (args[i].startsWith('-')) {
+      console.log(`${args[i].slice(1)} is ${args[i + 1]}`);
+    } else {
+      console.log(`${args[i]} is ${args[i + 1]}`);
+    }
   }
 };
 
