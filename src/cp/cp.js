0c4bf24439da7a763571a1c511c414cee1f9a4b6
Alex2 <aleksander1802@gmail.com>
2023-06-15 21:19:27 +0300

    feat: cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
+import { spawn } from 'child_process';
+import { resolve } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToFile = resolve('src/cp/files/script.js');
+
+  const child = spawn('node', [pathToFile, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  child.stdin.write(JSON.stringify(args));
+  child.stdin.end();
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
