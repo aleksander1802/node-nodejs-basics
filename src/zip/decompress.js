6b1c86c0e01042d6593e80a621cf4137b8a6565f
Alex2 <aleksander1802@gmail.com>
2023-06-15 16:07:08 +0300

    feat: zip completed

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { resolve } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+  const pathToDir = resolve('src/zip/files');
+  const source = createReadStream(resolve(pathToDir, 'archive.gz'));
+  const destination = createWriteStream(resolve(pathToDir, 'fileToCompress.txt'));
+  const gunzip = createGunzip();
+
+  try {
+    source.pipe(gunzip).pipe(destination);
+  } catch (error) {
+    if (error) {
+      console.error(`Error decompress: ${error}`);
+    }
+  }
 };
 
 await decompress();
