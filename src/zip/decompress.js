be1051e9eef93ae22cc90b7dae060776430df39a
Alex2 <aleksander1802@gmail.com>
2023-06-16 02:33:29 +0300

    feat: updated zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,15 +1,18 @@
 import { createGunzip } from 'node:zlib';
 import { createReadStream, createWriteStream } from 'node:fs';
 import { resolve } from 'path';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
 
 const decompress = async () => {
+  const pipe = promisify(pipeline);
   const pathToDir = resolve('src/zip/files');
   const source = createReadStream(resolve(pathToDir, 'archive.gz'));
   const destination = createWriteStream(resolve(pathToDir, 'fileToCompress.txt'));
   const gunzip = createGunzip();
 
   try {
-    source.pipe(gunzip).pipe(destination);
+    pipe(source, gunzip, destination);
   } catch (error) {
     if (error) {
       console.error(`Error decompress: ${error}`);
