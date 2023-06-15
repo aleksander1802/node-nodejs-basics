57f1dc1b7de3e4f7c1f7baab5fc4aa9e1e9afcfc
Alex2 <aleksander1802@gmail.com>
2023-06-15 23:26:01 +0300

    feat: updated cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
   const args = process.argv.slice(2);
 
   for (let i = 0; i < args.length; i += 2) {
-    console.log(`${args[i]} is ${args[i + 1]}`);
+    console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
   }
 };
 
