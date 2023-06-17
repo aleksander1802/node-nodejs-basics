15167f86a1c0ee114fd45eca70fb4ca51762a16d
Alex2 <aleksander1802@gmail.com>
2023-06-17 19:23:30 +0300

    feat: updated cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,27 +1,16 @@
-import { spawn } from 'child_process';
+import { fork } from 'child_process';
 import { resolve } from 'path';
 
 const spawnChildProcess = async (args) => {
   const pathToFile = resolve('src/cp/files/script.js');
 
-  const child = spawn('node', [pathToFile, ...args]);
-
-  process.stdin.pipe(child.stdin);
-
-  child.stdout.on('data', (data) => {
-    process.stdout.write(data);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+  const child = fork(pathToFile, [...args], { stdio: 'inherit' });
 
   child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    if (code !== 0) {
+      console.error(`Child process exited with code ${code}`);
+    }
   });
-
-  child.stdin.write(JSON.stringify(args));
-  
 };
 
 // Put your arguments in function call to test this functionality
